f1535b01fd9e551d16ea6d899a3e6efabdcd0801
Varya <53264874+Varya-Cherniavskaya@users.noreply.github.com>
2023-07-11 18:55:34 +0300

    Update hw_js_lesson4.js

--- hw_js_lesson4.js
+++ hw_js_lesson4.js
@@ -274,3 +274,245 @@
 // alert("Комп'ютер викинув: " + computerResult)
 
 // result == 0 ? alert('Ви програли') : (result == 1 ? alert("Ви перемогли") : alert("Нічия"))
+
+
+// //16.
+
+// let TaskName = prompt(`Введіть назву завдання, щоб запустити його код:
+//                         1. Number: odd
+//                         2. String: lexics
+//                         3. Boolean
+//                         4. Boolean: if
+//                         5. Comparison: sizes
+//                         6. Ternary
+//                         7. Training
+//                         8. Prompt: or
+//                         9. Confirm: or this days
+//                         10. Confirm: if this days
+//                         11. Default: or
+//                         12. Default: if
+//                         13. Login and password
+//                         14. Currency exchange
+//                         15. Scissors
+//                         16. Black belt`)
+
+// if (TaskName === "Number: odd" || 
+//     TaskName === "String: lexics" || 
+//     TaskName === "Boolean" || 
+//     TaskName === "Boolean: if" ||
+//     TaskName === "Comparison: sizes" || 
+//     TaskName === "Ternary" || 
+//     TaskName === "Training" || 
+//     TaskName === "Prompt: or" || 
+//     TaskName === "Confirm: or this days" || 
+//     TaskName === "Confirm: if this days" || 
+//     TaskName === "Default: or" || 
+//     TaskName === "Default: if" || 
+//     TaskName === "Login and password" || 
+//     TaskName === "Currency exchange" || 
+//     TaskName === "Scissors" || 
+//     TaskName === "Black belt"
+//     ) {
+
+// if (TaskName === "Number: odd") { //1
+//     let numb = prompt ("Введіть число")
+//     if (isNaN(numb)){
+//      alert ("Введіть будь-яке число цифрами")
+//     }
+//     else if (numb % 2 == 0){
+//         alert ("Число парне")
+//     }
+//     else {
+//         alert ("Число не парне")
+//     }
+// }
+
+// if (TaskName === "String: lexics") { //2
+//     let someText = prompt("Напишіть якийсь текст")
+//     alert (someText.indexOf("abcdef" && "12345"))
+// }
+
+// if (TaskName === "Boolean") { //3
+//     let message = confirm ("Підеш на вихідних у бар?") ? "На ранок буде боліти голова" : "Не будь таким нудним"
+//     alert (message)
+// }
+
+// if (TaskName === "Boolean: if") { //4
+//     let a = confirm ("Ваша стать? \"Ок\"-жіноча; \"Відміна\"-чоловіча")
+//     if (a){
+//         alert ("Ви жінка")
+//     }
+//     else {
+//         alert ("Ви чоловік")
+//     }
+// }
+
+// if (TaskName === "Comparison: sizes") { //5
+//     let a = prompt ("Ваш обсяг талії (у см)")
+
+//     if (a >= 66 && a <= 69){
+//         alert ("Ваш розмір у міжнарожній системі виміру XS");
+//     }
+//     else if (a >= 70 && a <= 74){
+//         alert ("Ваш розмір у міжнарожній системі виміру S");
+//     }
+//     else if (a >= 75 && a <= 78){
+//         alert ("Ваш розмір у міжнарожній системі виміру M");
+//     }
+//     else if (a >= 79 && a <= 83){
+//         alert ("Ваш розмір у міжнарожній системі виміру L");
+//     }
+//     else if (a >= 84 && a <= 89){
+//        alert ("Ваш розмір у міжнарожній системі виміру XL");
+//     }
+//     else if (a >= 90 && a <= 94){
+//        alert ("Ваш розмір у міжнарожній системі виміру XXL");
+//     }
+//     else if (a >= 95 && a <= 97){
+//        alert ("Ваш розмір у міжнарожній системі виміру XXXL");
+//     }
+// }
+
+// if (TaskName === "Ternary") { //6
+//     let a = confirm("Ваша стать: ok-чоловік, відміна-жінка") ? alert("Ви чоловік") : alert("Ви жінка")
+// }
+
+// if (TaskName === "Training") { //7
+//     let age = prompt ("Введіть ваш вік") || alert ("Помилка!")
+// }
+
+// if (TaskName === "Prompt: or") { //8
+//     let question = confirm ("шопінг?") || alert ("ти - бяка")
+// }
+
+// if (TaskName === "Confirm: or this days") { //9
+//     let question = confirm ("шопінг?")
+//     if (!question){
+//      alert ("ти - бяка")
+//     }
+// }
+
+// if (TaskName === "Confirm: if this days") { //10
+//     let question = confirm ("шопінг?")
+//     if (!question){
+//         alert ("ти - бяка")
+//     }
+// }
+
+// if (TaskName === "Default: or") { //11
+//     let firstName = prompt('Твоє ім\'я? чи будеш Іван')
+//     if (!firstName){
+//        firstName = 'Іван'
+//     }
+//     let secondName = prompt('Твоє по батькові? чи будеш Іванович')
+//     if (!secondName){
+//        secondName = 'Іванович'
+//     }
+//     let thirdName = prompt('Твоє прізвище? чи будеш Іванов')
+//     if (!thirdName){
+//        thirdName = 'Іванов'
+//     }
+//     alert(firstName + " " + secondName + " " + thirdName)
+// }
+
+// if (TaskName === "Default: if") { //12
+//     let firstName = prompt('Твоє ім\'я? чи будеш Іван')
+//     if (!firstName){
+//         firstName = 'Іван'
+//     }
+//     let secondName = prompt('Твоє по батькові? чи будеш Іванович')
+//     if (!secondName){
+//        secondName = 'Іванович'
+//     }
+//     let thirdName = prompt('Твоє прізвище? чи будеш Іванов')
+//     if (!thirdName){
+//         thirdName = 'Іванов'
+//     }
+//     alert(firstName + " " + secondName + " " + thirdName)
+// }
+
+// if (TaskName === "Login and password") { //13
+//     let userlogin = prompt ("Введіть логін")
+//     let userpassword = prompt ("Введіть пароль")
+//     if (userlogin !== login){
+//         alert ('Ви ввели невірний логін')
+//     }
+//     else if (userpassword !== password){ 
+//         prompt ('Ви ввели невірний пароль')
+//     }
+//     else{
+//         if (userlogin === login && userpassword === password){
+//             alert('Вітаю')
+//         }
+//     }
+// }
+
+// if (TaskName === "Currency exchange") { //14
+//     let usdToBuy = 35
+//     let eurToBuy = 40
+//     let usdToSale = 37
+//     let eurToSale = 42
+//     let summary
+
+//     let currency = prompt ('Введіть валюту (usd / eur)'.toLowerCase()) 
+//     if (currency === "usd" || currency === "eur"){
+//     let buyOrSale = confirm ('Купити або продати?')
+//     let rate = prompt ('Введіть суму на обмін:')
+//     if (buyOrSale){
+//      summary = (currency === "usd" ? (usdToSale * rate) : (eurToSale * rate))
+//     }
+//     else{
+//        summary = (currency === "usd" ? (rate * usdToBuy) : (rate * eurToBuy))
+//     }
+//     alert ("Результат: " + summary)
+//     }
+
+//     else {
+//         alert ('Невірна валюта')
+//     }
+// }
+
+// if (TaskName === "Scissors") { //15
+// let customResult = prompt ("Введіть 'к', 'н' чи 'б'")
+// let computerResult = Math.random ()
+// alert (computerResult)
+// if (customResult === "к" || customResult === "н" || customResult === "б"){
+// if (computerResult >= 0 && computerResult <= 0.33){
+//     alert ("Комп\'ютер згенерував: камінь")
+//     if (customResult === 'к') alert ('нічия')
+//     else if (customResult === "н") alert ('ви програли')
+//     else if (customResult === "б") alert ("ви перемогли")
+// }
+// if (computerResult > 0.33 && computerResult <= 0.66){
+//     alert ("Комп\'ютер згенерував: ножиці")
+//     if (customResult === 'н') alert ('нічия')
+//     else if (customResult === "б") alert ('ви програли')
+//     else if (customResult === "к") alert ("ви перемогли")
+// }
+// if (computerResult > 0.66 && computerResult <= 1){
+//     alert ("Комп\'ютер згенерував: бумагу")
+//     if (customResult === 'б') alert ('нічия')
+//     else if (customResult === "к") alert ('ви програли')
+//     else if (customResult === "н") alert ("ви перемогли")
+// }
+// }else{
+//     alert('ввели невірне значення')
+// }
+// }
+
+// if (TaskName === "Black belt") { //16
+//     let customResult = prompt ("Введіть 'к', 'н' чи 'б'")
+//     let computerResult = Math.random()
+//     let result
+
+//     ((customResult === 'к' && (computerResult > 0.33 && computerResult <= 0.66)) || (customResult === 'н' && computerResult > 0.66) || (customResult === 'б' && computerResult <= 0.33)) ? result = 1 : ((customResult === 'б' && (computerResult > 0.33 && computerResult <= 0.66)) || (customResult === 'к' && computerResult > 0.66) || (customResult === 'н' && computerResult <= 0.33)) ? result = 0 : result = 2
+
+//     alert("Комп'ютер викинув: " + computerResult)
+
+//     result == 0 ? alert('Ви програли') : (result == 1 ? alert("Ви перемогли") : alert("Нічия"))
+//     }
+// }
+
+// else {
+//     alert('Завдання з таким ім\'ям не існує')
+// }
